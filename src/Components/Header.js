ff7595bc421bfe97728875ab55c09087d33a4b0b
Pranav Raju <pranavraju2020@gmail.com>
2024-01-03 14:47:09 +0530

    slight changes

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,10 +1,12 @@
 import React from "react";
 import AccountCircle from "./AccountCircle";
-
+import { FaKeyboard } from "react-icons/fa";
 const Header = () => {
   return (
     <div className="header">
-      <div className="logo">LOOGO</div>
+      <div className="logo">
+      <FaKeyboard />
+      </div>
       <div className="user-icon">
         <AccountCircle/>
       </div>
